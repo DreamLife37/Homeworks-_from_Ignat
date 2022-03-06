eacb73d3bfb1129ac1c94b74cca8545d3a376764
Andrey <fail4iks@gmail.com>
2022-03-06 15:05:25 +0300

    Homework 5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import {PATH} from "./Routes";
+import s from './HW5.module.css'
+import {NavLink} from "react-router-dom";
+
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
+    return (<div className={s.header}>
+
+            <div className={`${s.item} ${s.activeLink}`}><NavLink to={PATH.PRE_JUNIOR}
+                                                                  className={({isActive}) => isActive ? `${s.activeLink}` : `${s.item}`}> PRE
+                JUNIOR </NavLink>
+            </div>
+
+            <div className={`${s.item} ${s.activeLink}`}><NavLink to={PATH.JUNIOR}
+                                                                  className={({isActive}) => isActive ? `${s.activeLink}` : `${s.item}`}> JUNIOR </NavLink>
+            </div>
+
+            <div className={`${s.item} ${s.activeLink}`}><NavLink to={PATH.JUNIOR_PLUS}
+                                                                  className={({isActive}) => isActive ? `${s.activeLink}` : `${s.item}`}> JUNIOR
+                PLUS</NavLink>
+            </div>
+
+            <div className={s.block}/>
 
         </div>
+
     )
 }
 
--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import s from './../HW5.module.css'
+import {NavLink} from "react-router-dom";
+import {PATH} from "../Routes";
 
 function Error404() {
     return (
-        <div>
+        <div className={s.pageError}>
             <div>404</div>
             <div>Page not found!</div>
             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+
+            <h4>Упс!</h4> Что-то пошло не так, этой страницы не существует. Вы можете вернуться на главную страницу.
+
+            <div className={s.item}><NavLink to={PATH.PRE_JUNIOR}>Main Page</NavLink>
+            </div>
+            <img src={"https://cdn-icons-png.flaticon.com/512/755/755014.png"}/>
+
         </div>
     )
 }
