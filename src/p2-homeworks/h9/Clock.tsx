5c10e5ff533fe0b230a6c58ff2085df1f084b4d7
Andrey <fail4iks@gmail.com>
2022-04-01 14:05:33 +0300

    Homework 9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,34 +1,50 @@
 import React, {useState} from 'react'
 import SuperButton from '../h3/SuperButton'
+import s from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearTimeout(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
+    console.log(timerId)
+    console.log(date)
+
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
+        setShow(false)
         // close
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+    const stringTime = date.toLocaleString('ru', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    })// fix with date
+
+    const stringDate = date.toLocaleString('ru', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        weekday: 'long',
+    }) // fix with date
+
 
     return (
-        <div>
+        <div className={s.clock}>
             <div
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
